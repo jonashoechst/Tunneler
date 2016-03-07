6c8945305deab399493f14483df3aff72565c55c
Jonas Höchst <git@jonashoechst.de>
2016-03-07 20:18:41 +0100

    Initial Commit

---
+++ Tunnler/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Tunnler
+//
+//  Created by Jonas Höchst on 07.03.16.
+//  Copyright © 2016 Jonas Höchst. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Tunnler/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Tunnler
+//
+//  Created by Jonas Höchst on 07.03.16.
+//  Copyright © 2016 Jonas Höchst. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
